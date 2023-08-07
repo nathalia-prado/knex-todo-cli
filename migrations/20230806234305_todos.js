64e4546e4c83d109a6f0f2dc05b767cac395e1bd
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-07 12:05:46 +1200

    add the first migration

---
+++ migrations/20230806234305_todos.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema
+      .createTable('todos', function (table) {
+          table.increments('id');
+          table.string('task');
+      })
+  
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema 
+    .dropTable("todos")
+}
