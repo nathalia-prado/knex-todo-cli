a3cb6f9437d8a01c523e85913089856e0131888f
Nathalia Prado <nathalia_898@hotmail.com>
2023-08-07 22:26:17 +1200

    add completed column

---
+++ migrations/20230807054409_add-completed-column.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema
+        .alterTable('todos', function (table) {
+            table.boolean('completed')
+    })
+  
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema
+        .alterTable('todos', function (table) {
+            table.dropColumn('completed')
+        })
+}
